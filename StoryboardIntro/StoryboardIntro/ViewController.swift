b390da1489546fb8a50b23b21f77dc7c056d61a1
Gayathri Aravindan <67168479+gayathriaravindan@users.noreply.github.com>
2021-07-14 14:07:11 -0400

    Add files via upload

--- StoryboardIntro/StoryboardIntro/ViewController.swift
+++ StoryboardIntro/StoryboardIntro/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Testing!")
+        
     }
 
-
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+        if let newTitle = textField.text {
+                appTitle.text = newTitle
+            }
+    }
+    
 }
-
