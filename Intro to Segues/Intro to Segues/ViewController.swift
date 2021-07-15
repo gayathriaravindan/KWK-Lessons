53d38f033bc0ce5688d4db437b53dd6cd7c4d9f5
Gayathri Aravindan <67168479+gayathriaravindan@users.noreply.github.com>
2021-07-15 11:47:20 -0400

    Add files via upload

---
+++ Intro to Segues/Intro to Segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intro to Segues
+//
+//  Created by Gayathri Aravindan on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
