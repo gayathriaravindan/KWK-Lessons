00e5fef7b642235e35eabaa94c45867603814fc1
Gayathri Aravindan <67168479+gayathriaravindan@users.noreply.github.com>
2021-07-20 13:26:54 -0400

    Add files via upload

---
+++ ToDo List/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Gayathri Aravindan on 7/20/21.
+//
+
+import UIKit
+
+class ToDoClass {
+
+   var description = ""
+   var important = false
+
+}
